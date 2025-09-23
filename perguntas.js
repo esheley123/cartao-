1a149dc04e3b1bb61e8024ccd5e38ee521c62dc9
esheley123 <s.esheley@escola.pr.gov.br>
2025-09-23 12:12:07 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -6,6 +6,76 @@ Shrek
 
 criaCartao(
 Shrek
- O que Shrek é?
- Ele é um ogro.
+ Qual o significado do nome shrek?
+ Significa terror ou monstro.
 )
+
+criaCartao(
+Shrek
+ Qual o significado do nome shrek?
+ Significa terror ou monstro.
+)
+
+criaCartao(
+Shrek
+ Quem é o melhor amigo de Shrek?
+ O Burro.
+)
+
+criaCartao(
+Shrek
+   Em que a Fiona se transforma após o por do sol?
+   Em ogro
+)
+
+criaCartao(
+Shrek
+    O que a Fiona é do Sherek?
+    Esposa
+)
+
+criaCartao(
+Shrek
+    Qual a cor do vestido de Fiona?
+    Verde
+)
+
+criaCartao(
+Shrek
+   Como burro e Shrek chegam ao casamento?
+    Cavalgam em um dragão.
+}
+
+criaCartao(
+Shrek
+   Qual o nome da poção que o gato de botas rouba do estoque da fada madrinha para tentar unir Shrek e Fiona?
+   Felizes para sempre.
+}
+
+criaCartao(
+Shrek
+ Quem é o melhor amigo de Shrek?
+ O Burro.
+)
+
+
+criaCartao(
+Shrek
+ Qual é a principal habilidade do Gato de botas?
+ Lutar com espada e seu charme. 
+}
+
+
+criaCartao(
+Shrek
+   Quantos filmes do Shrek existem?
+   3 filmes.
+}
+
+criaCartao(
+Shrek
+ Quem queria se casar com a Fiona?
+  O vilão Lorde Farquaad.
+}
+
+
